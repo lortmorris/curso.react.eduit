51fe748260f49c38697a9c810ff58646191cbf65
Cesar Casas <cesarcasas@bsdsolutions.com.ar>
2017-04-07 19:04:43 -0300

    fixes

--- class2/example/src/app/index.jsx
+++ class2/example/src/app/index.jsx
@@ -4,21 +4,20 @@ import { createStore } from 'redux';
 import {todo} from './reducers/index.jsx';
 
 class Title extends React.Component {
-
-  render(){
-    console.log(this, this.props.value);
-  return (
-    <h1>{this.props.value}</h1>
-  );
-  }
+    render(){
+        console.log(this, this.props.value);
+        return (
+            <h1>{this.props.value}</h1>
+        );
+    }
 }
 
 class App extends React.Component {
-  render() {
-    return (
-      <Title value="hola a todos" />
-    );
-  }
+    render() {
+        return (
+            <Title value="hola a todos" />
+        );
+    }
 }
 
 const appRender = ()=>render(<App />,
--- class2/example/src/app/reducers/todo.jsx
+++ class2/example/src/app/reducers/todo.jsx
@@ -1,23 +1,23 @@
 import {createStore} from 'redux';
 
 function todos(state=[], action){
-  switch(action.type){
-    case 'ADD_TODO':
-      return [...state, {
-        title: action.title,
-        status: false
-      }];
-    break;
+    switch(action.type){
+        case 'ADD_TODO':
+        return [...state, {
+            title: action.title,
+            status: false
+        }];
+        break;
 
-    case 'REMOVE_TODO':
-    return [...state.slice(0, actionindex)
-      .concat(state.slice(index+1))
-    ];
+        case 'REMOVE_TODO':
+        return [...state.slice(0, actionindex)
+            .concat(state.slice(index+1))
+        ];
 
-    break;
-    default:
-      return state;
-  }
+        break;
+        default:
+        return state;
+    }
 }
 
 
