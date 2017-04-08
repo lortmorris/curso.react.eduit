233bf8e4c9616e58344fea4e01128182b92e0e41
Cesar Casas <cesarcasas@bsdsolutions.com.ar>
2017-04-07 21:00:07 -0300

    class 2, components, style and props

---
+++ class2/example/src/app/components/SubTitle.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+class SubTitle extends React.Component{
+    render(){
+        return (
+            <h2>Todos 1</h2>
+        )
+    }
+}
+
+export default SubTitle;
--- class2/example/src/app/index.jsx
+++ class2/example/src/app/index.jsx
@@ -1,27 +1,51 @@
 import React from 'react';
 import { render } from 'react-dom';
-import { createStore } from 'redux';
-//import { todo } from './reducers/index.jsx';
+import SubTitle from './components/SubTitle.jsx';
 
-class Title extends React.Component {
-    render(){
-        console.log(this, this.props.value);
+class Title extends React.Component{
+    render(){        
         return (
-            <h1>{this.props.value}</h1>
-        );
+            <h1 style={{ color: this.props.color }}>{this.props.title}</h1>
+        )
     }
 }
+
+
+class Todos extends React.Component{
+    render(){
+        return (
+            <ul className="todos-box">
+                <li>Aprender React</li>
+            </ul>
+        )
+    }
+}
+
+class BlockTodos extends React.Component{
+    render(){
+        return (
+            <div>
+            <SubTitle />
+            <Todos />
+            </div>
+        )
+    }
+}
+
+
 
 class App extends React.Component {
     render() {
         return (
-            <Title value="hola a todos" />
+            <div>
+                <Title title="hola a todos los pibes" color="red" />
+                <BlockTodos />
+                <BlockTodos />
+            </div>
         );
     }
 }
 
-const appRender = ()=>render(<App />,
-document.getElementById('app')
-);
+const appRender = ()=> render( <App />, document.getElementById('app') );
 
 appRender();
