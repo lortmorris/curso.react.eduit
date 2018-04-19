23bd734f767092eda197b59d61f64d5e54f5a4cc
Cesar Casas <cesarcasas@bsdsolutions.com.ar>
2018-04-19 02:19:04 +0200

    task

--- class2/04-2018/index.js
+++ class2/04-2018/index.js
@@ -12,14 +12,40 @@ const render = () => {
 const store = Redux(reducer);
 store.observe(render);
 
-store.dispatch(actions.addTodo('task 1'));
-store.dispatch(actions.addTodo('task 2'));
-store.dispatch(actions.addTodo('task 3'));
+store.dispatch(actions.addTodosList('lista de tareas 1'));
+store.dispatch(actions.addTodo(0, 'task 1'));
+store.dispatch(actions.addTodo(0, 'task 2'));
+store.dispatch(actions.addTodo(0, 'task 3'));
+store.dispatch(actions.toggleTodo(0, 1)); // completed
 
-store.dispatch(actions.removeTodo(1));
-store.dispatch(actions.toggleTodo(0)); // completed
+store.dispatch(actions.addTodosList('Lista de tareas 2'));
+store.dispatch(actions.addTodo(1, 'Para hace 1'));
+store.dispatch(actions.addTodo(1, 'Para hace 2'));
+store.dispatch(actions.addTodo(1, 'Para hace 3'));
+store.dispatch(actions.toggleTodo(1, 2)); // completed
 
 /*
 
+state = {
+ todoslists: [
+  {
+  id: 0
+  title: 'todos title',
+  todos: [ { title: '', id: 0, completed: false } ],
+  }
+ ]
+}
+*/
+
+/*
+lista de tareas 1:
+  - [ ] Task 1
+  - [*] Task 2
+  - [ ] Task 3
+
+lista de tareas 2:
+  - [ ] Para hace 1
+  - [ ] Para hace 2
+  - [*] Para hace 3
 
 */
